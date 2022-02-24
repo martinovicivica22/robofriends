48973d11b2fd06d019389db89575c52849b8285f
martinovicivica22 <97768436+martinovicivica22@users.noreply.github.com>
2022-02-23 17:58:25 -0700

    src files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import "@fontsource/press-start-2p"
 
 ReactDOM.render(
   <React.StrictMode>
